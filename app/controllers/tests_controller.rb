210c091ca74959028b115c4337b6f045127ab8fb
luozh <luo_zhonghai@YG38-05939.local>
2015-02-05 15:16:47 +0900

    render to_json

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,10 +1,11 @@
 class TestsController < ApplicationController
   #get
   def show
-    @info = Info.new(data: 1, comment: "test");
+    @info = Info.new(data: 1, comment: "test")
   end
   #post
   def update
-    render :json => {info: 2, comment: "test2"}
+    @info = Info.new(data: 1, comment: "test")
+    render json: @info
   end
 end
